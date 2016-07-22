e7ba7b3fb6b040f5b56069fe364bb282bda6f5e5
benmcderm <benmcderm@gmail.com>
2016-07-22 14:03:25 -0700

    XHR

--- dropdown.js
+++ dropdown.js
@@ -1,3 +1,15 @@
+function getUrl(){
+  let str = window.location.href;
+  let re = /github.com(\/[^\/]+\/[^\/]+)[\/]?/;
+  let found = str.match(re);
+
+  return found[1];
+};
+
+function getIssuesUrl(){
+  return `${getUrl()}/issues`
+};
+
 function getSelectionText() {
     let text = "";
     if (window.getSelection) {
@@ -5,49 +17,49 @@ function getSelectionText() {
     } else if (document.selection && document.selection.type != "Control") {
         text = document.selection.createRange().text;
     }
-		sendIssue(text);
+		getUser();
 };
 
-function getIssuesUrl(){
-	return `${getUrl()}/issues`
-};
+function getUser() {
+  let xhr = new XMLHttpRequest();
 
-function getUrl(){
-	let str = window.location.href;
-	let re = /github.com(\/[^\/]+\/[^\/]+)[\/]?/;
-	let found = str.match(re);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        console.log(xhr.response);
+      } else if (xhr.status === 400) {
+        console.log("There was a 400 error.");
+      } else {
+        console.log("Something other than a 400 went wrong.");
+      }
+    }
+  };
 
-	return found[1];
-};
+  xhr.open("GET", `https://github.com/login/oauth/authorize?client_id=323a30406f9496db9815&scope=users`, true);
+  // xhr.setRequestHeader('Authorization', 'Basic ' + '74d93b944a289c48d44d8bb38f426b3cb7a239fc');
+  // xhr.onload = requestComplete;
+  xhr.send();
+}
 
-function revealDropdown() {
-	document.createElement(
-		"<div class=`gg-dropdown` style=`back` dropdown </div>"
-	);
-};
+function sendIssue(text) {
+  let xhr = new XMLHttpRequest();
 
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        console.log(xhr.responseText);
+      } else if (xhr.status === 400) {
+        console.log("There was a 400 error.");
+      } else {
+        console.log("Something other than a 400 went wrong.");
+      }
+    }
+  };
+
+  xhr.open("POST", `${getIssuesUrl()}`, true);
+  xhr.send(JSON.stringify({issue:{title: "Spelling Error", description:`${text}`}, username: "benmcderm"}));
+}
 
 
 document.onmousedown = getUrl;
 document.onmouseup = getSelectionText;
-
-function sendIssue(text) {
-	let xhr = new XMLHttpRequest();
-
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-			if (xhr.status === 200) {
-				console.log(xhr.responseText);
-			} else if (xhr.status === 400) {
-				console.log("There was a 400 error.");
-			} else {
-				console.log("Something other than a 400 went wrong.");
-			}
-		}
-	};
-
-	xhr.open("POST", `${getIssuesUrl()}`, true);
-	xhr.send(JSON.stringify({issue:{title: "Spelling Error", description:`${text}`}, username: "benmcderm"}));
-}
-
-// document.onmouseup = getComment;
